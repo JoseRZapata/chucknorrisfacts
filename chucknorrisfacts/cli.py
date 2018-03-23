458385f276da77433dccce29ebdf2a510f2860e1
Jose R. Zapata <jjrzg@hotmail.com>
2018-03-23 13:53:20 -0500

    Cambio CLI

--- chucknorrisfacts/cli.py
+++ chucknorrisfacts/cli.py
@@ -3,14 +3,16 @@
 """Console script for chucknorrisfacts."""
 import sys
 import click
+from chucknorrisfacts.chucknorrisfacts import cnfacts
 
 
 @click.command()
 def main(args=None):
     """Console script for chucknorrisfacts."""
-    click.echo("Replace this message by putting your code into "
-               "chucknorrisfacts.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    #click.echo("Replace this message by putting your code into "
+    #           "chucknorrisfacts.cli.main")
+    #click.echo("See click documentation at http://click.pocoo.org/")
+    cnfacts()
     return 0
 
 
